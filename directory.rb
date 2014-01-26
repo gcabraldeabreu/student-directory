9956cfb2c97664419589958bba8e52e803858bd3
Gabriel Cabral <gcabraldeabreu@gmail.com>
2014-01-26 21:53:40 +0000

    Added student list

---
+++ directory.rb
@@ -0,0 +1,12 @@
+puts "The Students of my cohort at Makers Academy"
+puts "---------------"
+puts "Bumblebee Man"
+puts "Disco Stu"
+puts "Dr. Nick Riveria"
+puts "Edna Krabappel"
+puts "Lionel Hutz"
+puts "Patty Bouvier"
+puts "Eleanor Abernathy"
+print "Overall, we have "
+print 6
+puts " outstanding students"
