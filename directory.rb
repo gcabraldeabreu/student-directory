e6ae3f54d7f91eab4bb386f1541927334b61bc44
Gabriel Cabral <gcabraldeabreu@gmail.com>
2014-01-27 22:35:19 +0000

    interactive directory

--- directory.rb
+++ directory.rb
@@ -1,28 +1,36 @@
-#here, we're placing the students into an array
-students = [
-	{:name => "Bumblebee Man", :cohort => :january},
-	{:name => "Disco Stu", :cohort => :january},
-	{:name => "Dr. Nick Riviera", :cohort => :january},
-	{:name => "Edna Krabappel", :cohort => :january},
-	{:name => "Lionel Hutz", :cohort => :january},
-	{:name => "Patty Bouvier", :cohort => :january},
-	{:name => "Eleanor Abernathy", :cohort => :january}
-]
+
 def print_header
   puts "The students of my cohort at Makers Academy"
   puts "-------------"
 end
-
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
+end
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	#create an empty array
+	students = []
+	#get the first name
+	name = gets.chomp
+	#while the name is not empty repeat this code
+	while !name.empty? do 
+		#add the student hash to the array
+		students << {:name => name, :cohort => :january}
+		#get another name from the user
+		name = gets.chomp
+  end
+  #return the array of students
+  students
 end
 
 def print_footer(names)
   puts "Overall, we have #{names.length} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
