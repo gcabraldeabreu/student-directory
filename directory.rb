c83965215f31d6ec31a28a9ac4b5bf9c2fd03588
Gabriel Cabral <gcabraldeabreu@gmail.com>
2014-01-27 22:19:15 +0000

    students cohort

--- directory.rb
+++ directory.rb
@@ -1,26 +1,28 @@
 #here, we're placing the students into an array
-students =[
-"Bumblebee Man",
-"Disco Stu",
-"Dr. Nick Riviera",
-"Edna Krabappel",
-"Lionel Hutz",
-"Patty Bouvier",
-"Eleanor Abernathy"
+students = [
+	{:name => "Bumblebee Man", :cohort => :january},
+	{:name => "Disco Stu", :cohort => :january},
+	{:name => "Dr. Nick Riviera", :cohort => :january},
+	{:name => "Edna Krabappel", :cohort => :january},
+	{:name => "Lionel Hutz", :cohort => :january},
+	{:name => "Patty Bouvier", :cohort => :january},
+	{:name => "Eleanor Abernathy", :cohort => :january}
 ]
 def print_header
-	puts "The students of my cohort at Makers Academy"
-	puts "============="
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
 
-def print(names)
-	names.each do |name|
-		puts name
-	end
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
-
 
 def print_footer(names)
-	puts "Overall, we have #{names.length} students"
+  puts "Overall, we have #{names.length} great students"
 end
 
+print_header
+print(students)
+print_footer(students)
